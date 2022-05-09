c83d5b3ee8ccd102aa96b035811c0f7fd7fca8ab
ARTOFGRAHAM <GREY.WOLF.COAT@GMAIL.com>
2022-05-09 01:21:23 +0100

    updated gem sentium and models

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,8 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # test "must save entry" do
+  #   entry = Entry.new
+  #   assert(entry.save)
+  # end
 end
